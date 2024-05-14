08f17c00de70a55a1d577470129d74b128c484e4
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-05-14 12:33:39 -0300

    Commit do sidebar flexivel

--- rotion/src/renderer/src/App.tsx
+++ rotion/src/renderer/src/App.tsx
@@ -1,16 +1,6 @@
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
 import './styles/global.css'
+import { Routes } from './Routes'
 
 export function App() {
-  return (
-    <div className="h-screen w-screen text-rotion-100 flex">
-      <Sidebar />
-      <div className="flex-1 flex-col max-h-screen"></div>
-      <Header />
-      <main className="flex-1 flex items-center justify-center text-rotation-400">
-        Seleccione ou crie um documento
-      </main>
-    </div>
-  )
+  return <Routes />
 }
--- rotion/src/renderer/tailwind.config.js
+++ rotion/src/renderer/tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['.src/**/*.tsx'],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       fontFamily: {
@@ -22,9 +22,26 @@ module.exports = {
           900: '#17141f',
         },
       },
+      keyframes: {
+        slideIn: {
+          from: { width: 0 },
+          to: { width: 'var(--radix-collapsible-content-width)' },
+        },
+
+        slideOut: {
+          from: { width: 'var(--radix-collapsible-content-width)' },
+          to: { width: 0 },
+        },
+      },
+
+      animation: {
+        slideIn: 'slideIn 0.25s',
+        slideOut: 'slideOut 0.25s',
+      },
     },
   },
   plugins: [
+    require('@tailwindcss/typography'),
     plugin(({ addUtilities }) => {
       addUtilities({
         '.region-drag': {
