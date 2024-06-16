d6fe7d506b0904cddc7a0f8fd20bca8295315bf7
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-06-15 22:29:16 -0300

    Commit de criacao de novo documento

--- rotion/src/renderer/src/components/Sidebar/CreatePage.tsx
+++ rotion/src/renderer/src/components/Sidebar/CreatePage.tsx
@@ -3,16 +3,16 @@ import { Plus } from 'phosphor-react'
 
 export function CreatePage() {
   const { isPending: isCreatingDocument, mutateAsync: createDocument } =
-    useMutation(async () => {
-      const response = await window.api.createDocument()
-
-      return response.data 
-
-  
+    useMutation({
+      mutationFn: async () => {
+        const response = await window.api.createDocument()
+        console.log('Response ', response)
+      },
+    })
 
   return (
     <button
-      onClick={() => createDocument}
+      onClick={() => createDocument()}
       disabled={isCreatingDocument}
       className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-rotion-600 hover:bg-rotion-700 disabled:opacity-60"
     >
